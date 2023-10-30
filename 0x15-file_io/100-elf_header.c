da235e8684027dd19ea21702a94fd0efe287a2ff
Chidinma9748 <chidinmaukachi20@gmail.com>
2023-10-30 14:27:33 -0700

    updated

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+Description:Checks whether a a file is an elf file
+
+readelf file is elf
+lseek elf file 
+
+printf 1 (elf file)
