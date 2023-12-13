33e9fe9d0fef280838512cc9b63de79d82638f30
Chidinma9748 <chidinmaukachi20@gmail.com>
2023-12-12 23:54:30 -0800

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer
+ * @n: The int to check
+ * Return: The absolute value of int
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+        int abs_val;
+	abs_val = n * -1;
+	return (abs_val);
+	}
+	return (n);
+}
+
